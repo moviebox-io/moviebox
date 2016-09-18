18b8b4066c3188285d4078a561190a94ffe895a0
Rui Posse <ruiposse@gmail.com>
2016-09-18 23:17:21 +0100

    (#1) express init + es6 + webpack + nodemon + debug + eslint

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Initial commit.
+`npm install`
+
+## backend
+
+* ES6 support - Using Babel.
+* Webpack - Backend bundle is created using webpack. Watches for changes in dev mode.
+* Auto server restart	- `nodemon` restarts the server in real-time anytime webpack updates the bundle.
+* Debugging via `debug` - Instead of console.log. If DEBUG env variable is not set, nothing is displayed to the console.
+* Code Linting	JavaScript code linting is done using ESLint
+
+```
+tab1
+`npm run server-dev`
+
+tab2
+`npm start`
+```
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// var models = require('./models');
+// var config = require('./config');
+// var express = require('express');
+// var app = express();
+import { version } from '../package.json';
+import express from 'express';
+const debug = require('debug')('moviebox');
+
+const api = express.Router(); // eslint-disable-line new-cap
+
+api.get('/', (req, res) => {
+  res.json({ version, test: 29 });
+});
+
+const app = express();
+app.use('/api', api);
+
+const server = app.listen(8000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  debug(`App listening at http://${host}:${port}`);
+});

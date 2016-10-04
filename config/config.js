cfd0555adc6b830d5ea2bdb576a633868551be89
David Picarra <david.picarra@accedo.tv>
2016-10-04 19:46:46 +0100

    feature: #5 react init with web pack config and package.json updated

--- config/config.js
+++ config/config.js
@@ -3,11 +3,19 @@ import path from 'path';
 const root = path.join(__dirname, '../');
 const backend = path.join(__dirname, '../backend');
 const nodeModules = path.join(__dirname, '../node_modules');
+const frontendSource = path.join(__dirname, '../frontend/');
+const frontendDist = path.join(__dirname, '../dist/frontend/');
 
 export default {
   root,
   backend,
   nodeModules,
+  frontend: {
+    src: frontendSource,
+    dist: frontendDist,
+    index: path.join(frontendSource, 'index.html'),
+    appRoot: path.join(frontendSource, 'app'),
+  },
   alias: {
     '@backend': backend,
   },

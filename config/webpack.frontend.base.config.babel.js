c6b7b0d2c7ebfbfadbde4c9e12afaec19dbf53e5
davidpicarra <daviddcp@gmail.com>
2016-10-05 11:04:21 +0100

    feature: #5 react init with web pack config and package.json updated (#14)

---
+++ config/webpack.frontend.base.config.babel.js
@@ -0,0 +1,29 @@
+import config from './config';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    config.frontend.appRoot,
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    path: config.frontend.dist,
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: config.frontend.src,
+    }],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: config.frontend.index,
+      inject: true,
+    }),
+  ],
+};
---
+++ frontend/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from './Greeting';
+
+ReactDOM.render(
+  <Greeting name="World?" />,
+  document.getElementById('app')
+);

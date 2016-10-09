1d0a7b00b217ce628c5a0f5cd0466af305ad65dd
David Picarra <david.picarra@accedo.tv>
2016-10-09 10:53:19 +0100

    feature: #5 HMR working, added module.hot.accept to avoid reloading page

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Greeting from './Greeting';
 
+// https://webpack.github.io/docs/hot-module-replacement-with-webpack.html#what-is-needed-to-use-it
+if (module.hot) {
+  module.hot.accept();
+}
+
 ReactDOM.render(
   <Greeting name="World?" />,
   document.getElementById('app')
